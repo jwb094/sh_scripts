79fedd9e5bd0a215435f5f4aa003ee1f52400e1e
Josh Bello <josh.bello@thirtythree.co.uk>
2024-07-12 11:51:49 +0100

    update (default_bs_new_bs.sh) added commenets and additional  logic to open file for editing

--- defaullt_bs_new_bs.sh
+++ defaullt_bs_new_bs.sh
@@ -10,5 +10,8 @@ touch "${SCRIPT_NAME}.sh"
 # Make the script file executable
 chmod +x "${SCRIPT_NAME}.sh"
 
-
+# display success msg stating the file has been created and can be run using bash command
 echo "New script created: ${SCRIPT_NAME}.sh and is executable "
+
+# open new script file to edit
+code "${SCRIPT_NAME}.sh"
